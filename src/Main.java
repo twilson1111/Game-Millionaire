57550f86cf4e0ed3a1536f1c4273c546346e132a
Xu Duo <leon_xenos.xu@foxmail.com>
2022-11-22 20:33:32 +0800

    db connector

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+import Database.DBConnector;
+import java.sql.Connection;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Connection connection = DBConnector.connect();
+        
+    }
+}
